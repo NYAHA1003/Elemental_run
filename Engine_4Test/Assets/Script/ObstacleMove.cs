5076c5d125b30b9c3c19105fc7b40f126f98d0a3
NYAHA_1003 <hsk5stars@gmail.com>
2022-06-23 00:08:15 +0900

    0623_0007
    장애물이 점점 빨라지고 색도 변한다

--- Engine_4Test/Assets/Script/ObstacleMove.cs
+++ Engine_4Test/Assets/Script/ObstacleMove.cs
@@ -4,37 +4,52 @@ using UnityEngine;
 
 public class ObstacleMove : MonoBehaviour
 {
+    [SerializeField]
+    private Material[] obsMaterial;
     private Rigidbody rigidBody;
-    public int obsNum = 0;
+    private int obsNum = 0;
     private float delay;
+    private float moveSpd;
+    private MeshRenderer meshRen;
 
     public void True()
     {
         gameObject.SetActive(true);
+        moveSpd -= 5f;
     }
 
     void Start()
     {
+        moveSpd = Mathf.Clamp(moveSpd, -125, -25);
         rigidBody = GetComponent<Rigidbody>();
+        meshRen = GetComponent<MeshRenderer>();
     }
 
     void Update()
     {
-        delay = Mathf.Clamp(delay, 0, 5);
-        delay = Random.Range(0f, 5f);
+        delay = Mathf.Clamp(delay, 0, 4);
+        delay = Random.Range(0f, 4f);
         Move();
+    }
+
+    public void SetProperty()
+    {
+        gameObject.transform.position = new Vector3(transform.position.x, transform.position.y, 100);
+        GameManager.instance.score += GameManager.instance.plusScore;
+        gameObject.SetActive(false);
+        obsNum = Random.Range(0, 4);
+        Debug.Log(obsNum);
+        ChangeColor(obsNum);
+        Invoke("True", delay);
     }
 
     public void Move()
     {
-        rigidBody.velocity = new Vector3(0,0,-25f);
+        rigidBody.velocity = new Vector3(0,0,moveSpd);
 
         if (transform.position.z <= -15)
         {
-            gameObject.transform.position = new Vector3(transform.position.x, transform.position.y, 100);
-            GameManager.instance.score += GameManager.instance.plusScore;
-            gameObject.SetActive(false);
-            Invoke("True", delay);
+            SetProperty();
         }
     }
 
@@ -42,12 +57,14 @@ public class ObstacleMove : MonoBehaviour
     {
         if (itemNum == 0 || itemNum == obsNum)
         {
-            Debug.Log("Ãæµ¹");
-            gameObject.transform.position = new Vector3(transform.position.x, transform.position.y, 100);
-            GameManager.instance.score += GameManager.instance.plusScore;
-            gameObject.SetActive(false);
-            Invoke("True", delay);
+            SetProperty();
         }
         else return;
+    }
+
+    public void ChangeColor(int obs)
+    {
+        Debug.Log(obs);
+        meshRen.material = obsMaterial[obs];
     }
 }
